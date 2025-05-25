7fd8789d8b44c3216700bd02c75a889ac75086cf
mudittyagi09 <muditbarla@gmail.com>
2025-05-25 08:28:28 +0530

    fix the error

--- src/Components/DashboardMainContent/DashboardMainContent.jsx
+++ src/Components/DashboardMainContent/DashboardMainContent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './DashboardMainContent.module.css';
-import DashboardOverview from '../DashboardOverview/DashboardOverview';
+import DashboardOverview from '../Dashboardoverview/Dashboardoverview';
 import CalendarView from '../CalendarView/CalendarView';
 import UpcomingSchedule from '../UpcomingSchedule/UpcomingSchedule';
 import ActivityFeed from '../ActivityFeed/ActivityFeed';
